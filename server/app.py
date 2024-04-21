acf9a9a611860d00501e64deb8893c71993f5c1d
nathaniEL mujesia <nathanielmujesia@gmail.com>
2024-04-21 16:53:22 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -17,11 +17,38 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    # GET
+    def get(self):
+        plants = [plant.to_dict() for plant in Plant.query.all()]
+        return make_response(plants, 200)
+    # POST
+    def post(self):
+        data=request.get_json()
+        new_plant = Plant(
+            name= data['name'],
+            image= data['image'],
+            price=data['price']
+        )
+        
+        db.session.add(new_plant)
+        db.session.commit()
+        
+        response_dict = new_plant.to_dict()
+        
+        return make_response(response_dict,201)
+    
+api.add_resource(Plants, '/plants')
+
+
 
 class PlantByID(Resource):
-    pass
+    def get(self,id):
+        plant = Plant.query.filter(Plant.id==id).first().to_dict()
         
+        return make_response(plant,200)
+        
+
+api.add_resource(PlantByID,'/plants/<int:id>')      
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
